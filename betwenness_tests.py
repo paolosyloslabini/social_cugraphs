512853f3be9317c1ff2ae76866a160db8c3b3825
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-06-09 09:25:31 +0200

    Create betwenness_tests.py

---
+++ betwenness_tests.py
@@ -0,0 +1,17 @@
+import cugraph
+
+# read data into a cuDF DataFrame using read_csv
+gdf = cudf.read_csv("graph_data.csv", names=["src", "dst"], dtype=["int32", "int32"])
+
+# We now have data as edge pairs
+# create a Graph using the source (src) and destination (dst) vertex pairs
+G = cugraph.Graph()
+G.from_cudf_edgelist(gdf, source='src', destination='dst')
+
+# Let's now get the PageRank score of each vertex by calling cugraph.pagerank
+df_page = cugraph.pagerank(G)
+
+# Let's look at the PageRank Score (only do this on small graphs)
+for i in range(len(df_page)):
+	print("vertex " + str(df_page['vertex'].iloc[i]) +
+		" PageRank is " + str(df_page['pagerank'].iloc[i]))
