11756343ca785ab5d7b6c8fcd938989e1ce029da
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-06-09 14:07:51 +0200

    Update relable_graph.py

--- relable_graph.py
+++ relable_graph.py
@@ -11,6 +11,7 @@ def relabel_graph(infilename, outfilename, mapfilename):
       if (row[0] not in idx_map):
         idx_map[row[0]] = idx;
         idx += 1;
+  print(idx_map);
   with open(infilename, 'r') as csv_file:
     with open(outfilename, 'w') as out_csv_file:
       writer = csv.writer(out_csv_file)
