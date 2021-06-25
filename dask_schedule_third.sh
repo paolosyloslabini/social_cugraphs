d08cfb5a6d9d2b1a1a671db3968e64f224f7595d
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-06-25 16:58:00 +0200

    Update dask_schedule_third.sh

--- dask_schedule_third.sh
+++ dask_schedule_third.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 #SBATCH -J dask-client
-#SBATCH -n 1
 #SBATCH -t 00:10:00
-#SBATCH --error=cuda.err
-
-
+#SBATCH --error=client-%j.err
+#SBATCH --output=client-%j.out
+#SBATCH --partition gpu
+#SBATCH --gres=gpu
+#SBATCH --nodes 1
 module load cuda-11.0
 module load anaconda3
-
 CONDA_ROOT=$(conda info --base)
 source $CONDA_ROOT/etc/profile.d/conda.sh
+conda activate rapids-21.06
+LOCAL_DIRECTORY=/home/clusterusers/kohofer/slurm/sylos/dask-local-dir
 
-conda activate rapids-0.19
 
-LOCAL_DIRECTORY=/nfs/dask-local-directory
+python3 /home/clusterusers/kohofer/slurm/sylos/dask-local-dir/dask-cudf-example.py
+ 
 
-cat <<EOF >>/tmp/dask-cudf-example.py
-import cudf
-import dask.dataframe as dd
-from dask.distributed import Client
 
-client = Client(scheduler_file="$LOCAL_DIRECTORY/scheduler.json")
-cdf = cudf.datasets.timeseries()
-
-ddf = dd.from_pandas(cdf, npartitions=10)
-res = ddf.groupby(['id', 'name']).agg(['mean', 'sum', 'count']).compute()
-print(res)
-EOF
-
-python3 /tmp/dask-cudf-example.py
 
