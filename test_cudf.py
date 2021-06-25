436ce645dd2f0df7fb21d72d277e7ce959459e95
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-06-25 17:48:50 +0200

    Create test_cudf.py

---
+++ test_cudf.py
@@ -0,0 +1,11 @@
+import cudf
+import dask.dataframe as dd
+from dask.distributed import Client
+schedulerjson = "/home/clusterusers/pasyloslabini/dask-local-directory/dask-scheduler.json"
+print(schedulerjson)
+client = Client(scheduler_file=schedulerjson)
+cdf = cudf.datasets.timeseries()
+
+ddf = dd.from_pandas(cdf, npartitions=10)
+res = ddf.groupby(['id', 'name']).agg(['mean', 'sum', 'count']).compute()
+print(res)                                                                                                                                                                                     ~                                                 
