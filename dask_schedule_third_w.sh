672583f9f7badb9a0b2e4dcc8e889410db1005af
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-07-05 23:05:38 +0200

    Create dask_schedule_third_w.sh

---
+++ dask_schedule_third_w.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH -J dask-client
+#SBATCH -t 48:00:00
+#SBATCH --error=client-%j.err
+#SBATCH --output=client-%j.out
+#SBATCH --partition gpu
+#SBATCH --gres=gpu
+#SBATCH --nodes 2
+module load cuda-11.0
+module load anaconda3
+CONDA_ROOT=$(conda info --base)
+source $CONDA_ROOT/etc/profile.d/conda.sh
+conda activate rapids-21.06
+
+python3 /home/clusterusers/pasyloslabini/social_cugraphs/analyse_weighted_graph.py --input-file ${1} --output-folder ${2}
