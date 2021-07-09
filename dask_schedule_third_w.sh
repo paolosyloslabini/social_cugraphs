cd66575ce6eac0db2a4a2971a0c57e2bf3b088c7
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-07-09 14:22:49 +0200

    Update dask_schedule_third_w.sh

--- dask_schedule_third_w.sh
+++ dask_schedule_third_w.sh
@@ -5,11 +5,11 @@
 #SBATCH --output=client-%j.out
 #SBATCH --partition gpu
 #SBATCH --gres=gpu
-#SBATCH --nodes 2
+#SBATCH --nodes 1
 module load cuda-11.0
 module load anaconda3
 CONDA_ROOT=$(conda info --base)
 source $CONDA_ROOT/etc/profile.d/conda.sh
 conda activate rapids-21.06
 
-python3 /home/clusterusers/pasyloslabini/social_cugraphs/analyse_weighted_graph.py --input-file /home/clusterusers/pasyloslabini/social_cugraphs/graphs/viterbo_muPOLYMOD_EP1_t500_all.ncol --output-folder /home/clusterusers/pasyloslabini/social_cugraphs/covid_graph/
+python3 /home/clusterusers/pasyloslabini/social_cugraphs/analyse_weighted_graph.py --input-file ${1} --output-folder ${2}
