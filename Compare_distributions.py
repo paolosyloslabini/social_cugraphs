72092466ae62543455ccf39ff8a0376ef8067a4e
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2021-07-19 17:14:57 +0200

    Update Compare_distributions.py

--- Compare_distributions.py
+++ Compare_distributions.py
@@ -2,25 +2,34 @@ import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import import glob
 
 if __name__ == "__main__":
         parser = argparse.ArgumentParser(description="Makes images of the distributions")
-        parser.add_argument("--input-file", default="/", help="the input csv of a graph property to be analyzed")
+        parser.add_argument("--input-folder", default="/", help="the input csvs to be analyzed")
         parser.add_argument("--output-folder", default="/", help="where to save images")
         parser.add_argument("--name", default="distribution_image", help="name of the graph")
         args = parser.parse_args()
-        input_file = args.input_file
+        input_folder = args.input_folder
         output_folder = args.output_folder
         name = args.name
 
-df = pd.read_csv(input_file, header = 0, names = ["index","value", "node"]) 
-  
-sorted_data = np.sort(df["value"].tolist())
+        
+for file in glob.glob(input_folder + "*.csv"):
+        
+        prop_name = name + "_" + file.split('.')[0]
+        
+        try:
+                df = pd.read_csv(file, header = 0, names = ["index","value", "node"]) 
+        except:
+                df = pd.read_csv(file, sep = " ", header = 0, names = ["node", "value"])
+                
+        sorted_data = np.sort(df["value"].tolist())
+        
+        # Cumulative counts:
+        plt.step(sorted_data, np.arange(sorted_data.size))  # From 0 to the number of data points-1
 
-# Cumulative counts:
-plt.step(sorted_data, np.arange(sorted_data.size))  # From 0 to the number of data points-1
-
-#plt.legend(bbox_to_anchor=(0.5, 1.00), shadow=True, ncol=4, fontsize=15)
-plt.title(name);   
-plt.savefig(output_folder + name + ".jpg", format = 'jpg', dpi=300, bbox_inches = "tight")
+        #plt.legend(bbox_to_anchor=(0.5, 1.00), shadow=True, ncol=4, fontsize=15)
+        plt.title(prop_name);   
+        plt.savefig(prop_name + ".jpg", format = 'jpg', dpi=300, bbox_inches = "tight")
 
